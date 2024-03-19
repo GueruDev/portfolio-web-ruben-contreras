15f7c1ae286a1bf85b535ea6e65b77048112c6f0
Gueru <rubencontrerasguerrero@hotmail.com>
2024-03-19 00:57:43 -0500

    fix: Inicio bugged, attending to fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './',
+  base: 'https://guerudev.github.io/portfolio-web-ruben-contreras/',
   plugins: [react()],
 });
