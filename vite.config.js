7c353828263cdc76fedb9d254ae13fa5cab04c2c
Gueru <rubencontrerasguerrero@hotmail.com>
2024-03-19 01:03:07 -0500

    fix:Next try

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'https://guerudev.github.io/portfolio-web-ruben-contreras/',
+  base: '/portfolio-web-ruben-contreras/',
   plugins: [react()],
 });
